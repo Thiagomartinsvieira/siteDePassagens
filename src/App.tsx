6fe9ed205f2bd26974baf62c313db1f6b5c3392a
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-07-22 18:27:34 -0300

    import home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Nav from './components/Nav';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Nav />
+        <Home />
       </BrowserRouter>
     </div>
   );
