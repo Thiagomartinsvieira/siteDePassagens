1db6efbe16b0d70a706c9ca076e647ef25de39b0
ThiagoMartinsVieira <thiagoalagadico@gmail.com>
2023-07-22 21:45:10 -0300

    components added benefits and destinations

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Nav from './components/Nav';
 import Home from './components/Home';
+import Destinations from './components/Destinations';
+import Benefits from './components/Benefits';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <BrowserRouter>
         <Nav />
         <Home />
+        <Destinations />
+        <Benefits />
       </BrowserRouter>
     </div>
   );
